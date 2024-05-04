253e9130d493eed070e4531395fa3ee0568d1c97
AnishHazra <anishhazra108@gmail.com>
2024-05-04 15:55:40 +0530

    about section done

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from "react";
+import "./about.css"; // Import CSS file for styling
+import image from "../assets/img/smilingwomen.jpg"
+import Header from '../components/header'
 
-export default function About() {
-    return (
-        <div>about</div>
-    )
-}
+
+const About = () => {
+  return (
+    <main id="background-main">
+    <Header />
+    <section className="hero-section">
+      <div className="container grid grid-two-column">
+        <div className="section-hero-image">
+          <picture>
+            <img src={image} alt="image" className="hero-img" />
+          </picture>
+        </div>
+
+        <div className="section-hero-data">
+          <h1 className="hero-heading">About Elderly Connect</h1>
+          
+          <p className="hero-para">
+            The Elders Care app is designed to provide comprehensive support and assistance to senior citizens, ensuring their well-being and quality of life. With a user-friendly interface, the app offers a range of features to enhance the lives of elders and their caregivers.
+          </p>
+
+          <div className="features-section">
+            <h2>Features included:</h2>
+            <ul>
+              <li>Access to curated resources for senior health, wellness, and lifestyle.</li>
+              <li>Communication tools to connect with family, friends, and caregivers.</li>
+            </ul>
+          </div>
+
+          <div className="features-section">
+            <h2>Features about to include:</h2>
+            <ul>
+              <li>Personalized health tracking and reminders for medications and appointments.</li>
+              <li>Emergency contact and SOS functionality for immediate assistance.</li>
+              <li>Social engagement through community forums and event listings.</li>
+              <li>Access to curated resources for senior health, wellness, and lifestyle.</li>
+              <li>Communication tools to connect with family, friends, and caregivers.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    </main>
+  );
+};
+
+export default About;
